a0fb19494873e5bf4f6bce26ff7caa20566aa03f
Ramazanyergun <yergunr@gmail.com>
2024-07-28 18:16:51 +0300

    Arayüz Tasarımı

---
+++ app/src/main/java/com/probox3d/stajprojesi/MainActivity.java
@@ -0,0 +1,47 @@
+package com.probox3d.stajprojesi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageButton button;
+    private Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        button = findViewById(R.id.button); // Eğer ImageButton kullanıyorsanız
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCamera();
+            }
+        });
+    }
+
+    private void openCamera() {
+
+        // Bu metot, kamera açma işlemini gerçekleştirecek
+        intent = new Intent(MainActivity.this, cameraActivity.class);
+        startActivity(intent);
+    }
+}
